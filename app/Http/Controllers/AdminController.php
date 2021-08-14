5fa0f9569b3bdd87d1a0d32ef48aac5196b9325d
Aziz Almazbek uulu <azizalmazbekuulu@gmail.com>
2021-08-14 13:09:22 +0600

    bug fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,7 +64,7 @@ class AdminController extends Controller
 
     public function confirmRegistration(Request $request, User $user)
     {
-        if ($request->code === $user->avatar)
+        if ($request->code === intval($user->avatar))
         {
             $user->registered_at = time();
             $user->avatar = null;
